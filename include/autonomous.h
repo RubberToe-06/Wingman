28de45a0180d040350be346ed12777aa2195ebc9
Drew Marino <drew.marino01@gmail.com>
2023-08-24 13:36:42 -0500

    Added autonomous drivetrain functions

--- include/autonomous.h
+++ include/autonomous.h
@@ -1,6 +1,40 @@
 #include "vex.h"
 #include "robot-config.h"
 
+// All functions starting with "drive" are shorthands for repitition's sake
+
+// Shorthand for setting drivetrain velocities
+void driveVelocities(float number){
+  FLM.setVelocity(number, pct);
+  FRM.setVelocity(number, pct);
+  BLM.setVelocity(number, pct);
+  BRM.setVelocity(number, pct);
+}
+
+// Shorthand for stopping the drivetrain
+void driveStop(){
+  FLM.stop();
+  FRM.stop();
+  BLM.stop();
+  BRM.stop();
+}
+
+// Shorthand for activating the drivetrain
+void driveSpins(directionType FLMr, directionType FRMr, directionType BLMr, directionType BRMr){
+  FLM.spin(FLMr);
+  FRM.spin(FRMr);
+  BLM.spin(BLMr);
+  BRM.spin(BRMr);
+}
+
+// Shorthand for setting a drivetrain stopping mode
+void driveModes(brakeType type){
+  FLM.setStopping(type);
+  FRM.setStopping(type);
+  BLM.setStopping(type);
+  BRM.setStopping(type);
+}
+
 // Code for autonomous period (WIP)
 void auton(int version){
     switch (version)
