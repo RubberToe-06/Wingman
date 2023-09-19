5f078eef3d449cc1955272ef443405542e707d2e
Drew Marino <drew.marino01@gmail.com>
2023-09-19 10:22:30 -0500

    Added object-oriented selection screen code

--- include/start-screen.hpp
+++ include/start-screen.hpp
@@ -1,7 +1,60 @@
 #include "vex.h"
 #include "robot-config.h"
+#include <vector>
 
 int auton;
+
+class screenButton {
+public:
+    int X_min;
+    int X_max;
+    int Y_min;
+    int Y_max;
+
+    static screenButton createScreenButton(int xMin, int xMax, int yMin, int yMax) {
+        screenButton button;
+        button.X_min = xMin;
+        button.X_max = xMax;
+        button.Y_min = yMin;
+        button.Y_max = yMax;
+        return button;
+    }
+
+private:
+    bool X_check() {
+        if (Brain.Screen.xPosition() > X_min && Brain.Screen.xPosition() < X_max) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    bool Y_check() {
+        if (Brain.Screen.yPosition() > Y_min && Brain.Screen.yPosition() < Y_max) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+public:
+    bool isPressing() {
+        if (X_check() && Y_check()) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+};
+
+screenButton auton1Button = screenButton.createScreenButton(0, 120, 0, 120);
+screenButton auton2Button = screenButton.createScreenButton(360, 480, 0, 120);
+screenButton auton3Button = screenButton.createScreenButton(0, 120, 120, 240);
+screenButton auton4Button = screenButton.createScreenButton(360, 480, 120, 240);
+screenButton auton5Button = screenButton.createScreenButton(120, 360, 0, 240);
 
 // Handles the screen printing logic for every possible selection
 // @param selectedAuton which auton the driver selected
@@ -47,19 +100,19 @@ void startScreenInit(){
     Brain.Screen.drawImageFromFile("selection-screen.png", 0, 0);
     waitUntil(Brain.Screen.pressing());
 
-    if (Brain.Screen.xPosition() < 120 && Brain.Screen.yPosition() < 120){
+    if (auton1Button.isPressing()){
         selectedLogic(1);
     }
-    else if (Brain.Screen.xPosition() < 120 && Brain.Screen.yPosition() > 120){
+    else if (auton2Button.isPressing()){
         selectedLogic(3);
     }
-    else if (Brain.Screen.xPosition() > 360 && Brain.Screen.yPosition() < 120){
+    else if (auton3Button.isPressing()){
         selectedLogic(2);
     }
-    else if (Brain.Screen.xPosition() > 360 && Brain.Screen.yPosition() > 120){
+    else if (auton4Button.isPressing()){
         selectedLogic(4);
     }
-    else if (Brain.Screen.xPosition() > 120 && Brain.Screen.xPosition() < 360){
+    else if (auton4Button.isPressing()){
         selectedLogic(5);
     }
 }
